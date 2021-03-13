df1df60f7d9ac69d1ae4b596b50804d06719c898
Karlson Drendel <kdrendel99@gmail.com>
2021-03-13 13:19:21 -0800

    Linted code and deleted duplicate of main.js

--- main.js
+++
@@ -1,1 +0,0 @@
-import{ beepBoop } from './beepBoop.js'
--- src/age.js
+++ src/age.js
@@ -1,71 +1,71 @@
 export default class Age {
-  constructor(years,expectancy) {
+  constructor(years, expectancy) {
     this.years = years;
     this.expectancy = expectancy;
   }
-findEarthAge(){
-  let earthAge = this.years * 1
-  return earthAge;
-};
-findMercuryAge(){
-  let mercuryAge = this.years * .24.toFixed(2)
-  return mercuryAge;
-};
-findVenusAge(){
-  let venusAge = this.years * .62.toFixed(2)
-  return venusAge;
-};
-findMarsAge(){
-  let marsAge = this.years * 1.88.toFixed(2)
-  return marsAge;
-};
-findJupiterAge(){
-  let jupiterAge = this.years * 11.86.toFixed(2)
-  return jupiterAge;
-};
-//---------------------------------------------------
-//---------------------------------------------------
-earthExpectancy(){
-  if (this.years > this.expectancy){
-    let outlived = this.years - this.expectancy
-    return outlived 
+  findEarthAge() {
+    let earthAge = this.years * 1;
+    return earthAge;
   }
-  return this.expectancy;
-}
-mercuryExpectancy(){
-  let earthYearsLeft = this.expectancy - this.years;
-  let mercExpect = earthYearsLeft * .24.toFixed(2);
-  if (this.years > this.expectancy){
-    let outlived = (this.years - this.expectancy) * .24; 
-    return outlived;
+  findMercuryAge() {
+    let mercuryAge = this.years * .24.toFixed(2);
+    return mercuryAge;
   }
-  return mercExpect;
-}
-venusExpectancy(){
-  let earthYearsLeft = this.expectancy - this.years;
-  let venusExpect = earthYearsLeft * .62.toFixed(2);
-  if (this.years > this.expectancy){
-    let outlived = (this.years - this.expectancy) * .62;
-    return outlived
-}
-  return venusExpect;
-}
-marsExpectancy(){
-  let earthYearsLeft = this.expectancy - this.years;
-  let marsExpect = earthYearsLeft * 1.88.toFixed(2);
-  if (this.years > this.expectancy){
-    let outlived = (this.years - this.expectancy) * 1.88;
-    return outlived
+  findVenusAge() {
+    let venusAge = this.years * .62.toFixed(2);
+    return venusAge;
   }
-  return marsExpect;
-}
-jupiterExpectancy(){
-  let earthYearsLeft = this.expectancy - this.years;
-  let jupiterExpect = earthYearsLeft * 11.86.toFixed(2);
-  if (this.years > this.expectancy){
-    let outlived = (this.years - this.expectancy) * 11.86;
-    return outlived
+  findMarsAge() {
+    let marsAge = this.years * 1.88.toFixed(2);
+    return marsAge;
   }
-  return jupiterExpect;
-}
+  findJupiterAge() {
+    let jupiterAge = this.years * 11.86.toFixed(2);
+    return jupiterAge;
+  }
+  //---------------------------------------------------
+  //---------------------------------------------------
+  earthExpectancy() {
+    if (this.years > this.expectancy) {
+      let outlived = this.years - this.expectancy;
+      return outlived;
+    }
+    return this.expectancy;
+  }
+  mercuryExpectancy() {
+    let earthYearsLeft = this.expectancy - this.years;
+    let mercExpect = earthYearsLeft * .24.toFixed(2);
+    if (this.years > this.expectancy) {
+      let outlived = (this.years - this.expectancy) * .24;
+      return outlived;
+    }
+    return mercExpect;
+  }
+  venusExpectancy() {
+    let earthYearsLeft = this.expectancy - this.years;
+    let venusExpect = earthYearsLeft * .62.toFixed(2);
+    if (this.years > this.expectancy) {
+      let outlived = (this.years - this.expectancy) * .62;
+      return outlived;
+    }
+    return venusExpect;
+  }
+  marsExpectancy() {
+    let earthYearsLeft = this.expectancy - this.years;
+    let marsExpect = earthYearsLeft * 1.88.toFixed(2);
+    if (this.years > this.expectancy) {
+      let outlived = (this.years - this.expectancy) * 1.88;
+      return outlived;
+    }
+    return marsExpect;
+  }
+  jupiterExpectancy() {
+    let earthYearsLeft = this.expectancy - this.years;
+    let jupiterExpect = earthYearsLeft * 11.86.toFixed(2);
+    if (this.years > this.expectancy) {
+      let outlived = (this.years - this.expectancy) * 11.86;
+      return outlived;
+    }
+    return jupiterExpect;
+  }
 }
