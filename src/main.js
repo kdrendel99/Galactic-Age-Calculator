e0c42ea74899afe1fda1ed5d167578e5f601f4ba
Karlson Drendel <kdrendel99@gmail.com>
2021-03-24 20:28:35 -0700

    Fix npm start errors.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
-import $ from 'jquery';
+// import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Age from './js/age.js';
+// import Age from './js/age.js';
 import './css/styles.css';
+
+// Template Literal example: 
+// $('.showImageCaption').html(`${response2.explanation}`);
+
+// Note that the above is taken from my current Martian Weather Tracker project.
 
